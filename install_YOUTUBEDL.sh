3dead8e3bb89d63779ec3a02a35d3d1afd02c0f1
bauke molenaar <bauke.molenaar@gmail.com>
2019-12-27 02:00:44 +0100

    Update install_YOUTUBEDL.sh

--- install_YOUTUBEDL.sh
+++ install_YOUTUBEDL.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
+mkdir ~/bin
+cp dl-mp3 ~/bin/dl-mp3
+chmod a+rx ~/bin/dl-mp3
