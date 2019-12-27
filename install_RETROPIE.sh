807144e80c12efb5dd682ba962c1b9fc7c337e45
bauke molenaar <bauke.molenaar@gmail.com>
2019-12-27 01:21:08 +0100

    Update install_RETROPIE.sh

--- install_RETROPIE.sh
+++ install_RETROPIE.sh
@@ -2,3 +2,8 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo update-locale LC_ALL="en_US.UTF-8"
 sudo apt-get install -y git lsb-release
+cd
+git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+cd RetroPie-Setup
+chmod +x retropie_setup.sh
+sudo ./retropie_setup.sh
