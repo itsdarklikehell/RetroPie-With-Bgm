666c7a1675f64ff0f16052ca4cfab0215cd68405
bauke molenaar <bauke.molenaar@gmail.com>
2019-12-27 03:53:39 +0100

    Update install_RETROPIEEXTRAS.sh

--- install_RETROPIEEXTRAS.sh
+++ install_RETROPIEEXTRAS.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+INSTALLDIR=~/RetroPie-Extra
+if [ -d "$INSTALLDIR" ]; then
+echo "Directory $INSTALLDIR allready exists."
+fi
+if [ ! -d "$INSTALLDIR" ]; then
+echo "Directory $INSTALLDIR DOES NOT exist."
 cd ~
 git clone https://github.com/zerojay/RetroPie-Extra.git
 cd RetroPie-Extra/
 ./install-extras.sh
 cd ~/RetroPie-Setup
-sudo ./retropie_setup.sh
+fi
