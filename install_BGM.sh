846c670e6bb12c4aa6a80f1df73cfb0d79725e98
bauke molenaar <bauke.molenaar@gmail.com>
2019-12-27 03:39:21 +0100

    Update install_BGM.sh

--- install_BGM.sh
+++ install_BGM.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+INSTALLDIR=~/BGM.py
+if [ -f "$INSTALLDIR" ]; then
+echo "File $INSTALLDIR allready exists."
+fi
+if [ ! -f "$INSTALLDIR" ]; then
+sudo apt-get install mpg123
+cd ~
+MADMODDER(){
+git clone https://github.com/madmodder123/retropie_music_overlay
+retropie_music_overlay
+chmod +x BGM_Install.sh
+./BGM_Install.sh
+}
+MADMODDER
+fi
