6af7c094b69b759052732e9ee4cc3b4d514db749
bauke molenaar <bauke.molenaar@gmail.com>
2019-12-27 03:44:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+INSTALLDIR=~/RetroPie-With-Bgm
+if [ -d "$INSTALLDIR" ]; then
+echo "Directory $INSTALLDIR allready exists. PLEASE REMOVE IT FIRST!"
+fi
+if [ ! -d "$INSTALLDIR" ]; then
+echo "Directory $INSTALLDIR DOES NOT exists."
 cd ~
 git clone https://github.com/itsdarklikehell/RetroPie-With-Bgm
 cd RetroPie-With-Bgm
 chmod +x *.sh
+fi
 ./install_RETROPIE.sh
 ./install_YOUTUBEDL.sh
 ./install_BGM.sh
