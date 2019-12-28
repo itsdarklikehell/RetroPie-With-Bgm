fd282c587a85243a1efb246bdd0cfd66415807bf
bauke molenaar <bauke.molenaar@gmail.com>
2019-12-28 02:54:39 +0100

    Update install_RETROPIE.sh

--- install_RETROPIE.sh
+++ install_RETROPIE.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 source config.ini
-
+sudo apt-get install -y git lsb-release byobu 
 if [ -d "~/RetroPie-Setup" ]; then
-echo "Directory allready exists."
+echo "Directory ~/RetroPie-Setup allready exists. Skipping Download."
 fi
 if [ ! -d "~/RetroPie-Setup" ]; then
 echo "Directory ~/RetroPie-Setup DOES NOT exist."
 sudo apt-get update && sudo apt-get upgrade -y
 sudo update-locale LC_ALL="en_US.UTF-8"
-sudo apt-get install -y git lsb-release byobu 
 cd
 git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
 fi
