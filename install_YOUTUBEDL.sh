aca2ad5c84b141f50ba57525f4c0ef71c5422635
bauke molenaar <bauke.molenaar@gmail.com>
2019-12-28 02:05:51 +0100

    Update install_YOUTUBEDL.sh

--- install_YOUTUBEDL.sh
+++ install_YOUTUBEDL.sh
@@ -12,9 +12,14 @@ if [ ! -d "~/bin" ]; then
 mkdir ~/bin
 fi
 cp ~/RetroPie-With-BGM/dl-mp3 ~/bin/dl-mp3
-chmod a+rx ~/bin/dl-mp3
+cp ~/RetroPie-With-BGM/dl-mp3 ~/bin/dl-mp4
+chmod +x ~/bin/dl-mp3
+chmod +x ~/bin/dl-mp5
 if [ ! -d "~/RetroPie/BGM" ]; then
 mkdir ~/RetroPie/BGM
 fi
 cd ~/RetroPie/BGM
-dl-mp3 --output startup.mp3 https://youtu.be/OMm1RLF32ig
+dl-mp3 https://www.youtube.com/watch?v=IbFEEfNE1YQ&list=PL89Sygaj3zlF_aS6Eo7YwfnqGHsVpXxDy
+
+cd ~/RetroPie/splashscreens
+dl-mp4 https://www.youtube.com/watch?v=2ujP3NFQbGM&list=PLTpZXoSGu0v7vSpeSYmT77rQEnAX-14kG
